4bec64a78757fbd581fcbf7a3e9429369ca0ca63
Nichlas Torgersen <post@nichiatu.com>
2014-10-08 23:01:26 +0200

    Update vis_klasseliste.php

--- vis_klasseliste.php
+++ vis_klasseliste.php
@@ -2,8 +2,12 @@
 	$var4 = $_GET['klasseKode'];
 	$lines = file('D:\\Sites\\home.hbv.no\\phptemp\\885664-student.txt');
 	$lines = preg_grep("/" . $var4 . "/", $lines);
-
+	
 	foreach ($lines as $key => $value) 
-	{ 
-		echo $value . '<br>'; 
+	{
+		// explode value into array
+		$student = explode(';', $value);
+		foreach ($student as $key => $value) {
+			echo $value . '<br>';
+		}
 	}
