63a9fa90e6ba0d28c202e64222ee1a7dc50e5e6c
Wazes <Mads_klevjer@hotmail.com>
2014-10-08 22:40:50 +0200

    et eller annet

---
+++ vis_klasseliste.php
@@ -0,0 +1,10 @@
+<?php
+	$var4 = $_GET['klassekode'];
+	$lines = file('D:\\Sites\\home.hbv.no\\phptemp\\885664-student.txt');
+	$lines = preg_grep("/$var4/", $lines);
+
+	foreach($lines as $var1, $var2, $var3);
+		{
+		echo "$name<br>";
+	}	
+?>
